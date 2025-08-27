f4ff100e344d83f78c449a0722c74ffe495201c2
Juan Nicolas Vela Tovar <nicolasvela63@gmail.com>
2025-08-27 12:16:41 -0500

    feat(User): add UserUseCase registerUser

---
+++ domain/usecase/src/main/java/co/com/auth/usecase/user/UserUseCase.java
@@ -0,0 +1,27 @@
+package co.com.auth.usecase.user;
+
+import co.com.auth.model.role.gateways.RoleRepository;
+import co.com.auth.model.user.User;
+import co.com.auth.model.user.gateways.UserRepository;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+public class UserUseCase {
+
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+    public Mono<User> registerUser(User u) {
+        return roleRepository.findById(u.getRol().getIdRol())
+                .switchIfEmpty(Mono.error(new IllegalArgumentException("El rol no existe")))
+                .flatMap(role -> {
+                    User toSave = u.toBuilder().rol(role).build();
+                    return userRepository.existsByEmail(toSave.getEmail())
+                            .flatMap(exists -> exists
+                                    ? Mono.error(new IllegalStateException("El correo ya esta en uso"))
+                                    : userRepository.save(toSave));
+                });
+    }
+}
