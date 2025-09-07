86e4f6d41bd6b891d2342e0e83313c9fd8aaba79
Juan Nicolas Vela Tovar <nicolasvela63@gmail.com>
2025-09-07 18:29:44 -0500

    feat(User): add findByEmail to UserReactiveRepository

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/auth/r2dbc/UserReactiveRepository.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/auth/r2dbc/UserReactiveRepository.java
@@ -13,4 +13,5 @@ public interface UserReactiveRepository
         extends ReactiveCrudRepository<UserEntity, UUID>, ReactiveQueryByExampleExecutor<UserEntity> {
     Mono<Boolean> existsByEmail(String email);
     Mono<User> findByNumberDocument(String number);
+    Mono<UserEntity> findByEmail(String email);
 }
